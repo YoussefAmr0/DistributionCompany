eff923ec82356234df9821ecc1d5255dddcb0a7f
YoussefAmr0 <321213295@sha.edu.eg>
2024-05-15 16:24:24 +0300

    update of products images in seeder

--- database/seeders/ItemsSeeder.php
+++ database/seeders/ItemsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Items;
+use Illuminate\Support\Facades\Storage;
 
 class ItemsSeeder extends Seeder
 {
@@ -15,7 +16,36 @@ class ItemsSeeder extends Seeder
         $categories = ['Art', 'Measuring', 'Office', 'Papers', 'Pens', 'School', 'Toys'];
 
         foreach ($categories as $category) {
+            // Copy files to storage/app/public
+            $this->copyFilesToStorage($category);
+
+            // Create items
             Items::factory()->count(12)->create(['category' => $category]);
         }
     }
+
+    /**
+ * Copy files from public/img/products to storage/app/public
+ */
+private function copyFilesToStorage()
+{
+    $sourcePath = public_path('img/products');
+    $destinationPath = storage_path('app/public/products');
+
+    // Ensure the destination directory exists
+    Storage::makeDirectory('public/products');
+
+    // Get list of files in the directory
+    $files = scandir($sourcePath);
+
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..') {
+            $filePath = $sourcePath . '/' . $file;
+
+            // Copy file to storage/app/public
+            Storage::putFileAs('public/products', $filePath, $file);
+        }
+    }
+}
+
 }
